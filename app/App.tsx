0873bc8240f0d88936c87b935d89e47e8cd6fc7d
Michael La Dow <mpladow@gmail.com>
2024-05-26 19:11:25 +1000

    first commit

---
+++ app/App.tsx
@@ -0,0 +1,45 @@
+import Button from '@components/Button/Button';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeTab = createBottomTabNavigator();
+const App = () => {
+	return (
+		<NavigationContainer>
+			<HomeTab.Navigator>
+				<View style={styles.container}>
+					<View style={styles.main}>
+						<Text style={styles.title}>Hello Poo World</Text>
+						<Text style={styles.subtitle}>This is the first page of your app.</Text>
+					</View>
+					<Button />
+				</View>
+			</HomeTab.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		padding: 24,
+	},
+	main: {
+		flex: 1,
+		justifyContent: "center",
+		maxWidth: 960,
+		marginHorizontal: "auto",
+	},
+	title: {
+		fontSize: 64,
+		fontWeight: "bold",
+	},
+	subtitle: {
+		fontSize: 36,
+		color: "#38434D",
+	},
+});
---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Button = () => {
+  return (
+	 <Pressable>
+		<Text>button</Text>
+	 </Pressable>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({})
